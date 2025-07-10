279cc9413326c845d3c3f1dccb8d65e607dbb18e
Porygon5 <VALLIERMATHIS.PRO@GMAIL.COM>
2025-07-10 16:29:44 +0200

    #3 feat : Restructuration de la page index.html

---
+++ app/Views/index.php
@@ -0,0 +1,9 @@
+<?php
+// Inclure le header
+include_once('../../public/assets/components/header.html');
+?>
+
+<!-- Inclure le contenu de la page principale -->
+<?php
+include_once('../../public/assets/pages/index.html');
+?>
