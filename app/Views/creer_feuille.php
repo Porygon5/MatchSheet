594d8d407c19dc1300998e59d41399d01371345c
Porygon5 <porygon@Porygon.>
2025-07-11 16:27:08 +0200

    Ajout de la page de création d'une feuille de match

---
+++ app/Views/creer_feuille.php
@@ -0,0 +1,9 @@
+<?php
+// Inclure le header
+include_once __DIR__ . '/../../public/assets/components/header.php';
+?>
+
+<!-- Inclure le contenu de la page principale -->
+<?php
+include_once __DIR__ . '/../../public/assets/pages/creation_feuille.html';
+?>
